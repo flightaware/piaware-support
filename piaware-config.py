e85812c0d60649c782c3a9a463456c10179ad90b
Nicholas Wan <nicholas.wan@flightaware.com>
2024-09-09 13:14:00 -0500

    fix config issues

--- piaware-config.py
+++ piaware-config.py
@@ -3,12 +3,12 @@ from uuid import UUID
 
 
 class MetadataSettings():
-    def __init__(self, default: Any = None, setting_type: str = None, protect: str = None, sdonly: bool = None, network: str = None) -> None:
-		self.default = default
-		self.setting_type = setting_type
-		self.protect = protect
-		self.sdonly = sdonly
-		self.network = network
+    def __init__(self, default: any = None, setting_type: str = None, protect: str = None, sdonly: bool = None, network: str = None) -> None:
+        self.default = default
+        self.setting_type = setting_type
+        self.protect = protect
+        self.sdonly = sdonly
+        self.network = network
 
 class Metadata():
     settings: MetadataSettings = {}
@@ -21,79 +21,79 @@ class Metadata():
         self.force_mac_address = MetadataSettings(setting_type="MAC")
         self.allow_auto_updates = MetadataSettings(setting_type="bool", default=False)
         self.allow_manual_updates = MetadataSettings(setting_type="bool", default=False)
-		self.network_config_style = MetadataSettings(setting_type="network_config_style", default="buster", sdonly=True, network=True)
+        self.network_config_style = MetadataSettings(setting_type="network_config_style", default="buster", sdonly=True, network=True)
         
-		self.wired_network = MetadataSettings(setting_type="bool", default=True, sdonly=True, network=True)
-		self.wired_type = MetadataSettings(setting_type="network_type", default="dhcp", sdonly=True, network=True)
-		self.wired_address = MetadataSettings(sdonly=True, network=True)
-		self.wired_netmask = MetadataSettings(sdonly=True, network=True)
-		self.wired_broadcast = MetadataSettings(sdonly=True, network=True)
-		self.wired_gateway = MetadataSettings(sdonly=True, network=True)
-		self.wired_nameservers = MetadataSettings(default= ["8.8.8.8", "8.8.4.4"], sdonly=True, network=True)
+        self.wired_network = MetadataSettings(setting_type="bool", default=True, sdonly=True, network=True)
+        self.wired_type = MetadataSettings(setting_type="network_type", default="dhcp", sdonly=True, network=True)
+        self.wired_address = MetadataSettings(sdonly=True, network=True)
+        self.wired_netmask = MetadataSettings(sdonly=True, network=True)
+        self.wired_broadcast = MetadataSettings(sdonly=True, network=True)
+        self.wired_gateway = MetadataSettings(sdonly=True, network=True)
+        self.wired_nameservers = MetadataSettings(default= ["8.8.8.8", "8.8.4.4"], sdonly=True, network=True)
 
-		self.wireless_network = MetadataSettings(setting_type="bool", default=False, sdonly=True, network=True)
-
-
-		self.wireless_ssid = MetadataSettings(sdonly=True, network=True)
-		self.wireless_password = MetadataSettings(protect=True, sdonly=True, network=True)
-		self.wireless_type = MetadataSettings(setting_type="network_type", default="dhcp", sdonly=True, network=True)
-		self.wireless_address = MetadataSettings(sdonly=True, network=True)
-		self.wireless_broadcast = MetadataSettings(sdonly=True, network=True)
-		self.wireless_netmask = MetadataSettings(sdonly=True, network=True)
-		self.wireless_gateway =  MetadataSettings(sdonly=True, network=True)
-
-		self.wireless_nameserver = MetadataSettings(default = ["8.8.8.8", "8.8.4.4"], sdonly=True, network=True)
-		self.wireless_country = MetadataSettings(default = 00, type="country" sdonly=True, sdonly=True)
-
-		self.allow_dhcp_duic = MetadataSettings(default=True, setting_type="bool", sdonly=True, network=True)
-
-		self.http_proxy_host = MetadataSettings(network=True)
-		self.http_proxy_port = MetadataSettings(network=True)
-		self.http_proxy_user = MetadataSettings(network=True)
-		self.http_proxy_password = MetadataSettings(protect=True, network=True)
+        self.wireless_network = MetadataSettings(setting_type="bool", default=False, sdonly=True, network=True)
 
 
-		self.adept_serverhosts = MetadataSettings() # Come back to this
-		self.adept_serverport = MetadataSettings(setting_type="int", default=1200)
+        self.wireless_ssid = MetadataSettings(sdonly=True, network=True)
+        self.wireless_password = MetadataSettings(protect=True, sdonly=True, network=True)
+        self.wireless_type = MetadataSettings(setting_type="network_type", default="dhcp", sdonly=True, network=True)
+        self.wireless_address = MetadataSettings(sdonly=True, network=True)
+        self.wireless_broadcast = MetadataSettings(sdonly=True, network=True)
+        self.wireless_netmask = MetadataSettings(sdonly=True, network=True)
+        self.wireless_gateway =  MetadataSettings(sdonly=True, network=True)
 
-		self.rfkill = MetadataSettings(setting_type="bool", default=False, sdonly=True)
-		self.receiver_type = MetadataSettings(setting_type="receiver", default="rtlsdr")
+        self.wireless_nameserver = MetadataSettings(default = ["8.8.8.8", "8.8.4.4"], sdonly=True, network=True)
+        self.wireless_country = MetadataSettings(default = 00, setting_type="country", sdonly=True, network=True)
 
-		self.rtlsdr_device_index = MetadataSettings(default=False, sdonly=True)
+        self.allow_dhcp_duic = MetadataSettings(default=True, setting_type="bool", sdonly=True, network=True)
 
-		self.rtlsdr_ppm = MetadataSettings(setting_type = "int", default = 0, sdonly=True)
-		self.rtlsdr_gain = MetadataSettings(setting_type = "gain", default = "max", sdonly=True)
+        self.http_proxy_host = MetadataSettings(network=True)
+        self.http_proxy_port = MetadataSettings(network=True)
+        self.http_proxy_user = MetadataSettings(network=True)
+        self.http_proxy_password = MetadataSettings(protect=True, network=True)
 
-		self.beast_baudrate = MetadataSettings(setting_type = "int", sdonly=True)
-		self.radarcape_host = MetadataSettings(sdonly = True)
 
-		self.receiver_port = MetadataSettings(setting_type = "int", default = 30005)
-		self.allow_mode_ac = MetadataSettings(setting_type = "bool", default = True, sdonly=True)
-		self.allow_mlat = MetadataSettings(setting_type = "bool", default = True)
-		self.mlat_results = MetadataSettings(setting_type = "bool", default = True)
-		self.mlat_results_anon =  MetadataSettings(setting_type = "bool", default = True)
+        self.adept_serverhosts = MetadataSettings() # Come back to this
+        self.adept_serverport = MetadataSettings(setting_type="int", default=1200)
 
-		self.mlat_results_format =  MetadataSettings(default = "beast,connect,localhost:30104 beast,listen,30105 ext_basestation,listen,30106")
-		self.slow_cpu = MetadataSettings(default = "auto", sdonly = True)
+        self.rfkill = MetadataSettings(setting_type="bool", default=False, sdonly=True)
+        self.receiver_type = MetadataSettings(setting_type="receiver", default="rtlsdr")
 
-		self.adaptive_dynamic_range = MetadataSettings(setting_type="bool", default = True, sdonly=True)
-		self.adaptive_dynamic_range_target = MetadataSettings(setting_type="double", sdonly=True)
+        self.rtlsdr_device_index = MetadataSettings(default=False, sdonly=True)
 
-		self.adaptive_burst = MetadataSettings(setting_type="bool", default=False, sdonly=True)
-		self.adaptive_min_gain = MetadataSettings(setting_type="double", sdonly=True)
-		self.adaptive_max_gain = MetadataSettings(setting_type="double", sdonly=True)
+        self.rtlsdr_ppm = MetadataSettings(setting_type = "int", default = 0, sdonly=True)
+        self.rtlsdr_gain = MetadataSettings(setting_type = "gain", default = "max", sdonly=True)
 
-		self.enable_firehose = MetadataSettings(setting_type="bool", default = False)
-		self.allow_ble_setup = MetadataSettings(default = "auto", sdonly = True)
+        self.beast_baudrate = MetadataSettings(setting_type = "int", sdonly=True)
+        self.radarcape_host = MetadataSettings(sdonly = True)
 
-		self.uat_receiver_type = MetadataSettings(setting_type = "uat_receiver", default=None)
-		self.uat_receiver_host = None
-		self.uat_receiver_port = MetadataSettings(setting_type = "int", default = 30978)
-		self.uat_sdr_gain = MetadataSettings(setting_type = "gain", default = "max", sdonly = True)
-		self.uat_sdr_ppm = MetadataSettings(setting_type = "double", default = 0, sdonly = True)
-		self.uat_sdr_device = MetadataSettings(default = "driver=rtlsdr", sdonly = True)
+        self.receiver_port = MetadataSettings(setting_type = "int", default = 30005)
+        self.allow_mode_ac = MetadataSettings(setting_type = "bool", default = True, sdonly=True)
+        self.allow_mlat = MetadataSettings(setting_type = "bool", default = True)
+        self.mlat_results = MetadataSettings(setting_type = "bool", default = True)
+        self.mlat_results_anon =  MetadataSettings(setting_type = "bool", default = True)
 
-		self.use_gpsd = MetadataSettings(setting_type="bool", default = True)
+        self.mlat_results_format =  MetadataSettings(default = "beast,connect,localhost:30104 beast,listen,30105 ext_basestation,listen,30106")
+        self.slow_cpu = MetadataSettings(default = "auto", sdonly = True)
+
+        self.adaptive_dynamic_range = MetadataSettings(setting_type="bool", default = True, sdonly=True)
+        self.adaptive_dynamic_range_target = MetadataSettings(setting_type="double", sdonly=True)
+
+        self.adaptive_burst = MetadataSettings(setting_type="bool", default=False, sdonly=True)
+        self.adaptive_min_gain = MetadataSettings(setting_type="double", sdonly=True)
+        self.adaptive_max_gain = MetadataSettings(setting_type="double", sdonly=True)
+
+        self.enable_firehose = MetadataSettings(setting_type="bool", default = False)
+        self.allow_ble_setup = MetadataSettings(default = "auto", sdonly = True)
+
+        self.uat_receiver_type = MetadataSettings(setting_type = "uat_receiver", default=None)
+        self.uat_receiver_host = None
+        self.uat_receiver_port = MetadataSettings(setting_type = "int", default = 30978)
+        self.uat_sdr_gain = MetadataSettings(setting_type = "gain", default = "max", sdonly = True)
+        self.uat_sdr_ppm = MetadataSettings(setting_type = "double", default = 0, sdonly = True)
+        self.uat_sdr_device = MetadataSettings(default = "driver=rtlsdr", sdonly = True)
+
+        self.use_gpsd = MetadataSettings(setting_type="bool", default = True)
 
 
 class ConfigFile():
