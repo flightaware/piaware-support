059ed5b63545ccb742b95d188030817a947ce730
Nicholas Wan <nicholas.wan@flightaware.com>
2025-01-15 11:51:34 -0600

    Add more logging and enable/disable wifi to generate-network-config-bookworm.py Have generate-network-config.service run after NetworkManager

--- scripts/generate-network-config-bookworm.py
+++ scripts/generate-network-config-bookworm.py
@@ -30,8 +30,16 @@ def wireless_conn_file_template(new_uuid: str, ssid: str, psk: str):
     [proxy]"""
 
 def generate_network_config(config: ConfigGroup):
+    if config.get("wired-network"):
+        print("wired-network set to yes")
+    else:
+        print("wired-network set to no")
+        subprocess.run(["nmcli", "con", "down", "eth0"])
 
     if not config.get("wireless-network"):
+        print("wireless-network set to no")
+        subprocess.run(["nmcli","con","down","wireless"])
+        subprocess.run(["nmcli", "radio", "wifi", "off"])
         return
 
     with open(f"{SYS_CON_DIR}/wireless.nmconnection", "w") as conn_file:
@@ -44,6 +52,8 @@ def generate_network_config(config: ConfigGroup):
     subprocess.run(["sync"])
 
     if not os.path.exists(FIRSTBOOT_PATH):
+        print("Upping wireless")
+        subprocess.run(["nmcli", "radio", "wifi", "on"])
         subprocess.run(["nmcli", "con", "up", "wireless"])
         with open(FIRSTBOOT_PATH, "w") as f:
             f.write("Used by generate-network-config-bookworm.py to determine if it's being run for the first time after a bootup.")
