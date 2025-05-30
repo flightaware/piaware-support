1d60850146fd5690785e435c50acf964cb6870a4
Nicholas Wan <nicholas.wan@flightaware.com>
2025-05-30 07:26:07 -0500

    Revert "NetworkManager SSIDs also need the same escaping for \." Re-create on dedicated feature branch
    This reverts commit 3c3ff53ecbc2e8f213ed41cfa1b475f5e31038d7.

--- flightaware_piaware_config/tests/test_piaware_config.py
+++ flightaware_piaware_config/tests/test_piaware_config.py
@@ -4,18 +4,6 @@ import unittest
 from flightaware_piaware_config.src.flightaware_piaware_config.piaware_config import *
 from uuid import UUID
 
-class TestMetadataSettings(unittest.TestCase):
-    def test_initialization(seflf):
-        testm = MetadataSettings(IntegerProcessor)
-        assert testm.processor is not None
-        assert testm.setting_type is None
-        assert testm.default is None
-        assert testm.protect is None
-        assert testm.sdonly is None
-        assert testm.network is None
-        assert testm.network_manager_value is False
-        assert testm.deprecated is False
-
 class TestMetadata(unittest.TestCase):
     def test_get_setting(self):
         testm = Metadata()
@@ -229,17 +217,15 @@ class TestConfigFile(unittest.TestCase):
         # Te!st"\pas\s -> "Te!st\"\\pas\\s"
         assert testc.process_quotes("\"Te!st\\\"\\\\pas\\\\s\"") == "Te!st\"\\pas\\s"
 
-        assert testc.process_quotes("\"Te!st\\\"\\\\pas\\\\s\"", processing_for_nm=True) == "Te!st\"\\\\pas\\\\s"
-        assert testc.process_quotes("Password's with comma", processing_for_nm=True) == "Password's with comma"
-        assert testc.process_quotes("\"Password's with comma\" # comments", processing_for_nm=True) == "Password's with comma"
-        assert testc.process_quotes("'Something\" with apostrophe' # comments ", processing_for_nm=True) == "Something\" with apostrophe"
-        assert testc.process_quotes("\"   Password's   with      lot's   of spaces   \"", processing_for_nm=True) == "   Password's   with      lot's   of spaces   "
-        assert testc.process_quotes("\"'/()$&@\\\"-[]{}#%^*+\\\\\"", processing_for_nm=True) == "'/()$&@\"-[]{}#%^*+\\\\"
+        assert testc.process_quotes("\"Te!st\\\"\\\\pas\\\\s\"", processing_password=True) == "Te!st\"\\\\pas\\\\s"
+        assert testc.process_quotes("Password's with comma", processing_password=True) == "Password's with comma"
+        assert testc.process_quotes("\"Password's with comma\" # comments", processing_password=True) == "Password's with comma"
+        assert testc.process_quotes("'Something\" with apostrophe' # comments ", processing_password=True) == "Something\" with apostrophe"
+        assert testc.process_quotes("\"   Password's   with      lot's   of spaces   \"", processing_password=True) == "   Password's   with      lot's   of spaces   "
+        assert testc.process_quotes("\"'/()$&@\\\"-[]{}#%^*+\\\\\"", processing_password=True) == "'/()$&@\"-[]{}#%^*+\\\\"
 
     def test_parse_line(self):
-        testm = Metadata()
-        testm.settings["option"] = MetadataSettings(StrProcessor)
-        testc = ConfigFile("file", metadata = testm)
+        testc = ConfigFile("file")
 
         key, val = testc.parse_line("  option      # whiteout entry, updated by fa_piaware_config in settings")
         assert key == "option"
@@ -272,7 +258,7 @@ class TestConfigFile(unittest.TestCase):
 
     def test_parse_config_from_list(self):
         testm = Metadata()
-        testm.settings["test"] = MetadataSettings(IntegerProcessor, deprecated=True)
+        testm.settings["test"] = MetadataSettings(IntegerProcessor(), deprecated=True)
 
         test_cases = [
             {
